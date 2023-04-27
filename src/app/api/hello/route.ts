65e1d8ff6b92f5a249401ba14e96cd0e906cfabd
Amaralcode19 <lucasamaralap@outlook.com>
2023-04-27 08:20:33 -0300

    :wrench: minor-changes

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"; // Importe os tipos corretos para as requisições e respostas
 
-export async function GET(res: Response) {
+export async function Request(res: Response) {
   return new Response('Hello world!')
 }
