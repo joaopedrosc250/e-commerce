1c4bc2684b7001249dbc562ae79cd897cc617696
Amaralcode19 <lucasamaralap@outlook.com>
2023-04-27 09:06:09 -0300

    :wrench: minor-changes

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ message: 'Hello world!' });
+export async function GET(request: Request) {
+  return new Response('Hello, Next.js!')
 }
