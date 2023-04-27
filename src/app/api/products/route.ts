da846c0ab517a3a3bdfe031537e4009068f159bf
Amaralcode19 <lucasamaralap@outlook.com>
2023-04-27 16:08:42 -0300

    :package: add-crud

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,50 @@
+import {
+  createProduct,
+  deleteProduct,
+  getAllProduct,
+  getProduct,
+  updateProduct,
+} from "@/app/controllers/product.controller";
+
+import { Request, Response } from "express";
+
+export default async function handle(req: Request, res: Response) {
+  try {
+    switch (req.method) {
+      case "GET": {
+        if (req.query.id) {
+          const product = await getProduct(req.query.id);
+          return res.status(200).json(product);
+        } else {
+          const products = await getAllProduct();
+          return res.status(200).json(products);
+        }
+      }
+      case "POST": {
+        const { type, title, url, size, image, price, desc } = req.body;
+        const product = await createProduct(
+          type,
+          title,
+          url,
+          size,
+          image,
+          price,
+          desc
+        );
+        return res.status(200).json(product);
+      }
+      case "PUT": {
+        const { id, ...updateData } = req.body;
+        const product = await updateProduct(id, updateData);
+        return product.status(200).json(product);
+      }
+      case "DELETE": {
+        const { id } = req.body;
+        const product = await deleteProduct(id);
+        return res.status(200).json(product);
+      }
+    }
+  } catch (error: any) {
+    return res.status(500).json({ ...error, message: error.message });
+  }
+}
--- src/app/controllers/product.controller.ts
+++ src/app/controllers/product.controller.ts
@@ -1,11 +1,49 @@
-import { Request, Response } from "express"
-const { createProduct } = require("../repo/products.repo")
+export const getAllProduct = async () => {
+  const products = await prisma.product.findMany({});
+  return products;
+};
 
-exports.create = async (req: Request, res: Response) => {
-     try{
-        const product = await createProduct(req.body)
-        res.status(200).send(product)
-     } catch (e) {
-        res.status(400).send(e)
-     }
-}
+export const getProduct = async (id: any) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  return product;
+};
+
+export const createProduct = async (
+  type: string,
+  title: string,
+  url: string,
+  size: string,
+  image: string,
+  price: number,
+  desc: string
+) => {
+  const product = await prisma.product.create({
+    data: {
+      type,
+      title,
+      url,
+      size,
+      image,
+      price,
+      desc,
+    },
+  });
+  return product;
+};
+
+export const updateProduct = async (id: any, updateData: any) => {
+  const product = await prisma.product.update({
+    where: {
+      id,
+    },
+    data: { ...updateData },
+  });
+  return product;
+};
+
+export const deleteProduct = async (id: any) => {
+  const product = await prisma.product.delete({ where: { id } });
+  return product;
+};
