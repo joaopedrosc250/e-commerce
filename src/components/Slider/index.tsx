422d84b439f991cf1ccce9f9bfd5e9987641cab2
Amaralcode19 <lucasamaralap@outlook.com>
2023-05-02 15:04:57 -0300

    :wrench: minor-changes

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export const Slider = () => {
-
   const images = [
     "/model-5.jpg",
     "/model-8.jpg",
@@ -19,15 +17,13 @@ export const Slider = () => {
               className="h-full hover:scale-105 hover:pl-2 hover:pr-2 transition-all ease-in-out"
               key={image}
             >
-              <Link href={"/pageproduct"}>
-                <Image
-                  src={image}
-                  alt="image"
-                  width={1920}
-                  height={1080}
-                  className="w-full cursor-pointer h-full hover:rounded-3xl transition-all ease-in-out"
-                />
-              </Link>
+              <Image
+                src={image}
+                alt="image"
+                width={1920}
+                height={1080}
+                className="w-full cursor-pointer h-full hover:rounded-3xl transition-all ease-in-out"
+              />
             </div>
           ))}
         </div>
