408d2d01456cb717bf436742c32339ae43ed958a
Amaralcode19 <lucasamaralap@outlook.com>
2023-05-02 14:37:25 -0300

    :wrench: minor-changes

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
-import FilterSearch from "../FilterSearch";
 
 export const Search = () => {
   return (
     <div id="search-box" className="flex flex-row justify-center items-center">
-      <div className="flex box-border gap-2">
-        <FilterSearch />
-        <Link href="" className="fa fa-search scale-125"></Link>
+      <div className="flex justify-center items-center border-2 rounded-3xl p-2 focus:rounded-none">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="pl-1 outline-none"
+        />
+        <Link href="" className="fa fa-search scale-125 px-1"></Link>
       </div>
     </div>
   );
