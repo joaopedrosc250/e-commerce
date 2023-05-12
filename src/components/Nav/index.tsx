e38e4d94405a7d0fdc78f98bb710b6ef6ff3aff8
Amaralcode19 <lucasamaralap@outlook.com>
2023-05-12 11:55:50 -0300

    :package: major-changes

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,65 +2,76 @@
 
 import Link from "next/link";
 import { Search } from "../Search";
-import { Heart, ShoppingCart, User } from "@phosphor-icons/react";
+import { Heart, List, ShoppingCart, User, X } from "@phosphor-icons/react";
+import { useState } from "react";
 
 export const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="flex w-full bg-white p-2 text-black xs:flex-col xs:items-center xs:justify-center xs:gap-0 md:gap-0 lg:flex-row lg:items-center lg:justify-center lg:gap-56">
-      <div className="flex items-center justify-center xs:flex-col xs:gap-1 lg:flex-row lg:gap-40 ">
-        <div>
+    <nav className="w-full bg-white p-4 text-black">
+      <div className=" flex w-full  p-2  xs:flex-row xs:items-center xs:justify-center xs:gap-8 md:gap-0 lg:flex-row lg:items-center lg:justify-center lg:gap-56">
+        <div className="flex items-center justify-center xs:flex-col xs:gap-1 lg:flex-row lg:gap-40 ">
           <Link href="/" className="text-3xl">
             UrbanAvenue
           </Link>
-        </div>
-        <div className="p-4">
           <Search />
         </div>
-      </div>
-      <div className="flex items-end justify-center lg:flex-col ">
-        <nav className="flex gap-6 xs:flex-col xs:items-center xs:justify-end md:justify-center lg:flex-row lg:items-center lg:justify-center">
-          {/* <button title="button" className={`fa fa-bars scale-125 `}></button> */}
-          <div className="flex xs:flex-col xs:items-end xs:justify-end lg:flex-row-reverse lg:items-center lg:justify-center">
-            <ul
-              className={`flex xs:items-center xs:justify-center xs:gap-4 lg:items-center lg:justify-center lg:gap-6 
-                `}
-            >
-              <li>
-                <Link className="hover:underline" href="/shop">
-                  Categories
-                </Link>
-              </li>
-              <li>
-                <Link className="hover:underline" href="#">
-                  Sign in
-                </Link>
-              </li>
-              <li>
-                <Link className="font-bold hover:underline" href="/home/sign">
-                  Sign up
-                </Link>
-              </li>
-              <li>
-                <Link className="" href="/shop/cart">
-                  <ShoppingCart size={30} />
-                </Link>
-              </li>
-              <li>
-                <Link className="" href="/shop/fav">
-                  <Heart size={30} />
-                </Link>
-              </li>
-              {/* {
-                <li>
+
+        <div className="items-end justify-center gap-6 xs:hidden lg:flex lg:flex-row ">
+          <Link className="hover:underline" href="/shop">
+            Categories
+          </Link>
+          <Link className="hover:underline" href="#">
+            Sign in
+          </Link>
+          <Link className="font-bold hover:underline" href="/home/sign">
+            Sign up
+          </Link>
+          <Link className="" href="/shop/cart">
+            <ShoppingCart size={30} />
+          </Link>
+          <Link className="" href="/shop/fav">
+            <Heart size={30} />
+          </Link>
+          {/* {
                   <Link className="scale-125 " href="/home/profile/1">
                     <User size={30} />
                   </Link>
-                </li>
-              } */}
-            </ul>
-          </div>
-        </nav>
+        } */}
+        </div>
+        <div className="flex lg:hidden">
+          <button title="button" onClick={() => setMenuOpen(prev => !prev)}>
+            {menuOpen ? <X size={36} /> : <List size={36} />}
+          </button>
+        </div>
       </div>
-    </div>
+      {menuOpen && (
+        <div className="h-[calc(100vh - 101px)] absolute left-0 top-32 w-full bg-white lg:hidden">
+          <div className="flex flex-col items-center justify-center">
+            <Link className="hover:underline" href="/shop">
+              Categories
+            </Link>
+            <Link className="hover:underline" href="#">
+              Sign in
+            </Link>
+            <Link className="font-bold hover:underline" href="/home/sign">
+              Sign up
+            </Link>
+            <Link className="" href="/shop/cart">
+              <ShoppingCart size={30} />
+            </Link>
+            <Link className="" href="/shop/fav">
+              <Heart size={30} />
+            </Link>
+            {/* {
+                 <Link className="scale-125 " href="/home/profile/1">
+                   <User size={30} />
+                 </Link>
+       } */}
+          </div>
+        </div>
+      )}
+    </nav>
   );
 };
