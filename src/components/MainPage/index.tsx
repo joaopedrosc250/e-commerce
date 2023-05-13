d90aa19e370135b2dddfe6e7fa0a2cb5b8603f33
Amaralcode19 <lucasamaralap@outlook.com>
2023-05-13 13:30:44 -0300

    :wrench: minor-changes

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -8,24 +8,38 @@ function Main() {
   const [data, setData] = useState<ProductProps[]>([]);
   const [load, setLoad] = useState(true);
 
-  useEffect(() => {
-    setLoad(true);
-    fetch("/api/products")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoad(false);
-        console.log(data)
-      });
-  }, []);
+  fetch("/api/products")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setData(data);
+      setLoad(false);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 
   return (
     <Suspense>
       <div className="grid justify-items-center">
         <h3 className="text-xl font-bold xs:mt-0 lg:mt-12">T-shirts</h3>
         <div className="mt-6 grid w-5/6 items-center xs:grid-cols-1 xs:gap-20 md:grid-cols-2 lg:grid-cols-4 lg:gap-16">
-          {load && <p>Loading</p>}
-          {data.map((element) => (
+          {load && (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          )}
+          {data.slice(0, 4).map((element) => (
             <Product
               url={element.url}
               id={element.id}
@@ -44,47 +58,59 @@ function Main() {
       <div className="grid justify-items-center">
         <h3 className="mt-12 text-xl font-bold">Cap</h3>
         <div className="mt-6 grid w-5/6 items-center xs:grid-cols-1 xs:gap-20 md:grid-cols-2 lg:grid-cols-4 lg:gap-16">
-          {data
-            .filter((element) => element.type == "cap")
-            .slice(0, 4)
-            .sort(() => Math.random() - 0.5)
-            .map((element) => (
-              <Product
-                url={element.url}
-                id={element.id}
-                key={element.id}
-                image={element.image}
-                title={element.title}
-                price={element.price}
-                type={element.type}
-                size={element.size}
-                desc={element.desc}
-                stock={0}
-              />
-            ))}
+          {load && (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          )}
+          {data.slice(0, 4).map((element) => (
+            <Product
+              url={element.url}
+              id={element.id}
+              key={element.id}
+              image={element.image}
+              title={element.title}
+              price={element.price}
+              type={element.type}
+              size={element.size}
+              desc={element.desc}
+              stock={0}
+            />
+          ))}
         </div>
       </div>
       <div className="grid justify-items-center">
         <h3 className="mt-12 text-xl font-bold">Shoe</h3>
         <div className="mb-24 mt-6 grid w-5/6 items-center xs:grid-cols-1 xs:gap-20 md:grid-cols-2 lg:grid-cols-4 lg:gap-16">
-          {data
-            .filter((element) => element.type == "shoe")
-            .slice(0, 4)
-            .sort(() => Math.random() - 0.5)
-            .map((element) => (
-              <Product
-                url={element.url}
-                id={element.id}
-                key={element.id}
-                image={element.image}
-                title={element.title}
-                price={element.price}
-                type={element.type}
-                size={element.size}
-                desc={element.desc}
-                stock={0}
-              />
-            ))}
+          {load && (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          )}
+          {data.slice(0, 4).map((element) => (
+            <Product
+              url={element.url}
+              id={element.id}
+              key={element.id}
+              image={element.image}
+              title={element.title}
+              price={element.price}
+              type={element.type}
+              size={element.size}
+              desc={element.desc}
+              stock={0}
+            />
+          ))}
         </div>
       </div>
     </Suspense>
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -6,11 +6,12 @@ import { Heart, List, ShoppingCart, User, X } from "@phosphor-icons/react";
 import { useState } from "react";
 
 export const Nav = () => {
+  
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="w-full bg-white p-4 text-black">
-      <div className=" flex w-full  p-2  xs:flex-row xs:items-center xs:justify-center xs:gap-8 md:gap-0 lg:flex-row lg:items-center lg:justify-center lg:gap-56">
+      <div className=" flex w-full p-2 xs:flex-row xs:items-center xs:justify-center xs:gap-10 md:gap-[30vh] lg:flex-row lg:items-center lg:justify-center lg:gap-56">
         <div className="flex items-center justify-center xs:flex-col xs:gap-1 lg:flex-row lg:gap-40 ">
           <Link href="/" className="text-3xl">
             UrbanAvenue
@@ -41,14 +42,14 @@ export const Nav = () => {
         } */}
         </div>
         <div className="flex lg:hidden">
-          <button title="button" onClick={() => setMenuOpen(prev => !prev)}>
+          <button title="button" onClick={() => setMenuOpen((prev) => !prev)}>
             {menuOpen ? <X size={36} /> : <List size={36} />}
           </button>
         </div>
       </div>
       {menuOpen && (
         <div className="h-[calc(100vh - 101px)] absolute left-0 top-32 w-full bg-white lg:hidden">
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-2">
             <Link className="hover:underline" href="/shop">
               Categories
             </Link>
@@ -64,11 +65,9 @@ export const Nav = () => {
             <Link className="" href="/shop/fav">
               <Heart size={30} />
             </Link>
-            {/* {
-                 <Link className="scale-125 " href="/home/profile/1">
-                   <User size={30} />
-                 </Link>
-       } */}
+            {/* <Link className="scale-125 " href="/home/profile/1">
+              <User size={30} />
+            </Link> */}
           </div>
         </div>
       )}
