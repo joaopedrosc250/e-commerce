29d81851ae257108eddd4bbe1a60d9ac77a5f62b
Amaralcode19 <lucasamaralap@outlook.com>
2023-05-19 11:39:05 -0300

    :wrench: minor-changes

--- web/src/app/home/profile/[id]/page.tsx
+++ web/src/app/home/profile/[id]/page.tsx
@@ -7,7 +7,7 @@ import { User } from '@phosphor-icons/react'
 export default function Profile(props: any) {
   return (
     <div className="flex h-auto flex-col items-center justify-center bg-white text-black">
-      <Nav />
+      <Nav id={''} quantity={0} />
       <div className="relative mb-10 mt-10 flex h-screen flex-col items-center justify-start rounded-3xl border-2 border-slate-500 bg-slate-50 xs:w-3/4 lg:w-2/4">
         <div className="absolute left-0 top-0 ml-12 mt-8">
           <User size={42} className="" />
